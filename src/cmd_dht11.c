304cf71e03559f262b6ae873ce236e83c904b3d9
Andrew McDonnell <bugs@andrewmcdonnell.net>
2015-03-19 00:31:57 +1030

    Refactored the DHT-11 read code so that it actually works now. However, the timeout code is not, which means the MCU will reset if there is no device present...

--- src/cmd_dht11.c
+++ src/cmd_dht11.c
@@ -1,16 +1,15 @@
 /*
- * Adaptation of Mark Ruys Arduino-DHT library to the ESP8266 and
+ * Module to sample a DHT-11 temperature / humidity sensor from the ESP8266 for the
  * Necromant's Frankenstein firmware by Andrew McDonnell <bugs@andrewmcdonnell.net>
  * 
- * Mark Ruys <mark@paracas.nl> original source code:
- *   An Arduino library for reading the DHT family of temperature and humidity sensors.
- *   https://github.com/markruys/arduino-DHT
- *
  * See also:
+ *   http://embedded-lab.com/blog/?p=4333
  *   http://www.micro4you.com/files/sensor/DHT11.pdf
  *   http://www.adafruit.com/datasheets/DHT22.pdf
  *
  * Presently hard-coded to GPIO #2 (accessible on the ESP-01 module)
+ *
+ * TODO: extend to cover the DHT-22 module
  */
 #include "ets_sys.h"
 #include "os_type.h"
@@ -25,6 +24,7 @@
 #include "console.h"
 
 #include <stdlib.h>
+#include <math.h>
 #include <generic/macros.h>
 
 #ifdef CONFIG_CMD_DHT11_DEBUG
@@ -34,162 +34,136 @@
 #endif
 
 #define MINIMUM_POLL_DHT11 1000000   /* Minimum number of microseconds between polling the DHT11 (1Hz duty cycle) */
-#define MINIMUM_POLL_DHT22 2000000   /* Minimum number of microseconds between polling the DHT22 (1/2Hz duty cycle) */
 
 static void dht11_setup();
 static int dht11_read_sensor(float* temperature, float* humidity);
 
 static int do_dht11(int argc, const char* const* argv)
 {
-  float temperature, humidity;
+  float temperature, humidity; /* DHT-11 is only 8 bits decimal anyway, but allow for future extension to DHT-22 */
   int er;
-	dht11_setup();
+  dht11_setup();
   er = dht11_read_sensor(&temperature, &humidity);
   switch (er) {
   case -1:
-		console_printf( "No DHT family device found\n" );
+    console_printf( "No DHT-11 device found\n" );
     break;
   case -2:
-		console_printf( "Checksum error\n" );
+    console_printf( "Checksum error\n" );
     break;
   default:
-  	console_printf( "Temperature: %.1f Celsius, Humidity %.1f\n", temperature, humidity);
+    /* hmm.  seems printf(%f) doesnt work... */
+    console_printf( "Temperature: %d.%d Celsius, Humidity: %d\n",
+      (int)floorf(temperature), (int)(10.F*(temperature - floorf(temperature))), (int)floor(humidity));
     break;
   }
-	return er;
+  return er;
 }
 
 /* Setup to try and read a DHT11 or DHT22 on the specified gpio pin (currently, hard-coded to 2) */
 void dht11_setup(int gpio)
 {
-	/* set gpio2 as gpio pin, with pullup enabled. */
-	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-	PIN_PULLDWN_DIS(PERIPHS_IO_MUX_GPIO2_U);
-	PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO2_U);  
+  /* set gpio2 as gpio pin, with pullup enabled. */
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+  PIN_PULLDWN_DIS(PERIPHS_IO_MUX_GPIO2_U);
+  PIN_PULLUP_DIS(PERIPHS_IO_MUX_GPIO2_U); /* we expect an external pullup to Vcc */
+  GPIO_DIS_OUTPUT(2);
 
   dbg("Preparing to read a DHT-family device using gpio 2\n");
+}
+
+static os_timer_t data_timer;
+static volatile int dht11_data_timeout_flag;
+static void ICACHE_FLASH_ATTR
+dht11_data_timeout(void* unused)
+{
+  dht11_data_timeout_flag ++;
+}
+
+static uint8 dht11_read_byte(int gpio)
+{
+  /* assumes GPIO already set for reading */
+  /* assumes we got the start sequence & configured the timer properly */
+  uint16 bit;
+  uint16 byte = 0;
+  for (bit=0; bit<8; bit++)
+  {
+    /* wait until the bit transitions to high */
+    while (!GPIO_INPUT_GET(gpio) && !dht11_data_timeout_flag);
+    os_delay_us(40);
+    /* Sample the next bit */
+    if (GPIO_INPUT_GET(gpio)) { byte |= (1 << (7-bit)); }
+    /* wait to return to low */
+    /* if by some fluke the sensor is disconnected inside (40x~80us)==3.2ms... next line will hang without timer check */
+    while(GPIO_INPUT_GET(gpio) && !dht11_data_timeout_flag);
+  }
+  return byte & 0xff;
 }
 
 static int dht11_read_sensor(float* temperature, float* humidity)
 {
   /* For now, hard-coded to GPIO 2 */
   const int gpio=2;
+  uint16 checksum;
 
-  /* Start off as input... */
-	GPIO_DIS_OUTPUT(gpio);
+  /* Starting as INPUT (thus pulled up to VCC) */
+  os_delay_us(MINIMUM_POLL_DHT11);
 
-  /* Send a start signal.
-   * We try and auto-detect the device as follows:
-   * (1) first, try as if it is a DHT-22
-   * (2) If that times out assume we have a DHT-11 (if there is a device...)
-   * (3) If still timeout then assume error
-   */
+  /* set a timeout for data from the sensor. */
+  /* It actually only needs to be 80+80us but here we are using 5ms, so the same time can be used to detect a spurious disconnect */
+  os_timer_disarm(&data_timer);
+  os_timer_setfn(&data_timer, (os_timer_func_t *)dht11_data_timeout, NULL);
+  dht11_data_timeout_flag = 0;
 
-  /* We need to search in this order... */
-  enum { MODEL_DHT22 = 0, MODEL_DHT11, MODEL_NONE };
+  /* hold low 25ms, then high 30us (see http://embedded-lab.com/blog/?p=4333 )*/
+  GPIO_OUTPUT_SET(gpio, 0);
+  os_delay_us(25000);
+  os_timer_arm(&data_timer, 5, 0);  /* I Have tried values all the way down to 1 and the MCU still resets if no connection... */
 
-  uint16 readTemperature = 0;
-  uint16 readHumidity = 0;
+  GPIO_OUTPUT_SET(gpio, 1);
+  os_delay_us(30);
 
-  uint8 model;
-  uint16 data;
-  uint8 checksum;
-  for (model=0; model != MODEL_NONE; model++)
-  {
-
-    switch (model) {
-    case MODEL_DHT11: 
-      os_delay_us(MINIMUM_POLL_DHT11);
-      dbg("Trying model DTH11\n"); break;
-    case MODEL_DHT22: 
-      os_delay_us(MINIMUM_POLL_DHT22);
-      dbg("Trying model DTH22\n"); break;
-    }
-
-    /* pin low, then delay */
-  	GPIO_OUTPUT_SET(gpio, 0);
-    switch (model) {
-    case MODEL_DHT11: 
-      os_delay_us(18000); break;
-    case MODEL_DHT22: 
-      os_delay_us(800); break;
-    };
-
-    /* Read 83 edges:
-     *   First a FALLING, RISING, and FALLING edge for the start bit
-     *   Then 40 bits: RISING and then a FALLING edge per bit
-     *   To keep things simple, we accept any HIGH or LOW reading if it's max 85 usecs (~45 x 2us retries) long
-     * First 16 bits == humidity
-     * Next 16 bits == temperature
-     */
-
-  	GPIO_DIS_OUTPUT(gpio);
-    data = 0;
-    for (uint8 edge=-3 ; edge < 2 * 40; edge++ ) {
-      uint8 retries;
-      for (retries = 45; retries != 0; retries --)  {
-        // For odd egdes we need to read 0, for even edges 1
-        if (GPIO_INPUT_GET(gpio) == ((edge & 1)?1:0)) break;
-        os_delay_us(2);
-      }
-      /* Sometimes you just need to use goto */
-      if (retries == 0) goto read_failed;
-
-      if (edge >= 0 && (edge & 1) ) {
-        /* Shift in the bits so last read bit is LSB */
-        data <<= 1;
-        /* Low is max 30 us & high least 68 us */
-        if (retries * 2 > 30) {
-          data |= 1;
-        }
-      }
-
-      switch (edge) {
-      case 31: readHumidity = data; break;
-      case 63: readTemperature = data; data = 0; break;
-      /* On exit from loop data will contain checksum byte */
-      }
-    }
-    break;
-read_failed:
-    switch (model) {
-    case MODEL_DHT11: 
-      dbg("It is not a DTH11\n"); break;
-    case MODEL_DHT22: 
-      dbg("It is not a DTH22\n"); break;
-    }
-  }
-
-  if (model == MODEL_NONE) {
+  /* now poll the input until it goes high, or 256 us (> 80 + 80us is minimum) has elasped (timeout) */
+  GPIO_DIS_OUTPUT(gpio);
+  while (!GPIO_INPUT_GET(gpio) && !dht11_data_timeout_flag);
+  /* and we go low again... */
+  while (GPIO_INPUT_GET(gpio) && !dht11_data_timeout_flag);
+  if (dht11_data_timeout_flag) {
+    os_timer_disarm(&data_timer);
+    dbg("No sensor detected.\n");
     return -1;
   }
+  /** WARNING **/
+  /* WHEN NO CABLE IS CONNECTED: */
+  /* PRESENTLY, THE MCU RESETS AFTER A SECOND OR SO; FOR SOME REASON THE TIMEOUT IS NOT WORKING :-( */
 
-  checksum = (uint8)(((uint8)readHumidity) + (readHumidity >> 8) + ((uint8)readTemperature) + (readTemperature >> 8));
+  uint8 b0 = dht11_read_byte(gpio);
+  uint8 b1 = dht11_read_byte(gpio);
+  uint8 b2 = dht11_read_byte(gpio);
+  uint8 b3 = dht11_read_byte(gpio);
+  uint8 b4 = dht11_read_byte(gpio);
+  if (dht11_data_timeout_flag) {
+    os_timer_disarm(&data_timer);
+    dbg("Sensor interrupted!\n");
+    return -1;
+  }
+  os_timer_disarm(&data_timer);
 
-  dbg("Data: %.4x %.4x %.2x Checksum: %2x\n", readHumidity, readTemperature, data, checksum);
+  checksum = b0 + b1 + b2 + b3;
+  dbg("Data: %02x %02x %02x %02x %02x Checksum: %02x\n", b0, b1, b2, b3, b4, checksum);
 
-  if ( checksum != data ) {
+  if (checksum != b4) {
     return -2;
   }
 
-  switch (model) {
-  case MODEL_DHT11: 
-    *humidity = readHumidity >> 8;
-    *temperature = readTemperature >> 8;
-    break;
-  case MODEL_DHT22: 
-    *humidity = readHumidity * 0.1;
-    if ( readTemperature & 0x8000 ) {
-      readTemperature = -(sint16)(readTemperature & 0x7FFF);
-    }
-    *temperature = ((sint16)readTemperature) * 0.1;
-  }
+  *humidity = b0;
+  *temperature = b2;
   return 0;
 }
 
-CONSOLE_CMD(dht11, 2, 2, 
-	    do_dht11, NULL, NULL, 
-	    "Read temperature and humidity from DHT11 / DHT22 sensor module."
-	    HELPSTR_NEWLINE "dht11 <gpio>"
-	);
+CONSOLE_CMD(dht11, 1, 1, 
+      do_dht11, NULL, NULL, 
+      "Read temperature and humidity from DHT11 sensor module. Warning: will reset MCU if no device present!"
+      HELPSTR_NEWLINE "dht11 <gpio>"
+  );
 
