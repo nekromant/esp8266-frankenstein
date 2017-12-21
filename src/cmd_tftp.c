e34e681027518d230b44b12f8371192dff73a550
Andrew Andrianov <andrew@ncrmnt.org>
2017-12-21 21:04:41 +0300

    Add esp8266 ping library and fix compilation of tftp upgrades
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/cmd_tftp.c
+++ src/cmd_tftp.c
@@ -6,15 +6,12 @@
 #include "flash_layout.h"
 #include "espconn.h"
 #include "gpio.h"
-#include "driver/uart.h" 
+#include "driver/uart.h"
 #include "microrl.h"
 #include "env.h"
 #include "console.h"
 #include <stdarg.h>
 #include <generic/macros.h>
-#include <lwip/netif.h>
-#include <lwip/tcp.h>
-#include <lwip/udp.h>
 
 
 #include <lib/tftp.h>
@@ -22,25 +19,19 @@
 struct update_server {
 	struct tftp_server *ts;
 	char buffer[SPI_FLASH_SEC_SIZE];
-	int pos; 
+	int pos;
 	uint32_t numbytes;
 	uint32_t fblock;
-	int ready; 
+	int ready;
+    int reboot;
 	/*volatile*/ os_timer_t commit_timer;
 };
 
 static struct update_server *u = NULL;
 
-#if 0
-static  __attribute__ ((section(".iram0.text"))) void isr_dummy(void *a)
-{
-	console_printf("%p\n", a);
-}
-#endif
-
 static  __attribute__ ((section(".iram0.text"))) void commit_handler(void* a)
 {
-	int i; 
+	int i;
 
 	/* Inhibit any interrupts */
 
@@ -48,18 +39,21 @@ static  __attribute__ ((section(".iram0.text"))) void commit_handler(void* a)
 	uint32_t numsect = (u->numbytes >> (ffs(SPI_FLASH_SEC_SIZE) - 1));
 	numsect++;
 
-	console_printf("\nCommiting update, %d sectors %d bytes\n", numsect, u->numbytes);
-	
-	for (i=0; i < numsect; i++) { 
+	console_printf("\nComitting update, %d sectors %d bytes\n", numsect, u->numbytes);
+
+	for (i=0; i < numsect; i++) {
 		ets_uart_printf("#");
 		spi_flash_erase_sector( i );
 		spi_flash_read((u->fblock + i) * SPI_FLASH_SEC_SIZE,  (uint32*) u->buffer, SPI_FLASH_SEC_SIZE);
 		spi_flash_write((i * SPI_FLASH_SEC_SIZE), (uint32*) u->buffer, SPI_FLASH_SEC_SIZE);
 		spi_flash_erase_sector( u->fblock + i );
 	}
-	ets_uart_printf("\nFirmware update completed (%d sectors), rebooting\n", i);
-	ets_wdt_enable();
-	while (1);;; /* Hang on here */
+	if (u->reboot) {
+		ets_uart_printf("\nFirmware update completed (%d sectors), rebooting\n", i);
+		ets_wdt_enable();
+		while (1);;; /* Hang on here */
+	}
+	console_lock(0);
 }
 
 
@@ -68,30 +62,30 @@ void recv_cb(struct tftp_server *ts, int num_block, char* buf, int len)
 	bool islast = (len != 512);
 	int tocopy = min_t(int, len, SPI_FLASH_SEC_SIZE - u->pos);
 	memcpy(&u->buffer[u->pos], buf, tocopy);
-	u->pos+=tocopy; 
+	u->pos+=tocopy;
 
-	if (islast || (u->pos == SPI_FLASH_SEC_SIZE)) { 
+	if (islast || (u->pos == SPI_FLASH_SEC_SIZE)) {
 		console_printf("#");
 		if (0 == (u->numbytes % (10*4096)))
 			console_printf("\n ");
-				
-		u->pos=0;		
+
+		u->pos=0;
 		spi_flash_erase_sector(u->fblock);
-		spi_flash_write(u->fblock * SPI_FLASH_SEC_SIZE, (void *) u->buffer, 
+		spi_flash_write(u->fblock * SPI_FLASH_SEC_SIZE, (void *) u->buffer,
 				SPI_FLASH_SEC_SIZE);
 		u->fblock++;
 	}
 
 	len -=tocopy;
 	u->numbytes+=tocopy;
- 
+
 	/* Unaligned? */
 	if (len)
 		return recv_cb(ts, num_block, &buf[tocopy], len);
-	
-	if (islast) { 
+
+	if (islast) {
 		console_printf("\n TFTP done, %d bytes transferred\n", (int)u->numbytes);
-		console_printf("Committing update in 2 seconds\n");
+		console_printf("Comitting update in 2 seconds\n");
 		os_timer_disarm(&u->commit_timer);
 		os_timer_setfn(&u->commit_timer, (os_timer_func_t *) commit_handler, ts);
 		os_timer_arm(&u->commit_timer, 2000, 0);
@@ -107,35 +101,35 @@ static int  do_tftp(int argc, const char* const* argv)
 	}
 
 	u = os_malloc(sizeof(struct update_server));
-	if (!u) { 
+	if (!u) {
 		console_printf("tftp: out of memory!\n");
 		return 0;
 	}
 
 	u->ts = os_malloc(sizeof(struct tftp_server));
-	if (!u) { 
+	if (!u) {
 		console_printf("tftp: out of memory!\n");
 		goto errfreeu;
 	}
-	
+
 	tftp_start(u->ts, IPADDR_ANY, 69);
 	tftp_recv(u->ts, recv_cb);
 	const char *host = env_get("tftp-server");
-	const char *path = env_get("tftp-dir"); 
+	const char *path = env_get("tftp-dir");
 	const char *file = env_get("tftp-file");
-	if (!host) { 
+
+	if (!host) {
 		console_printf("Please set 'tftp-server' var\n");
 		goto errfreets;
 	}
-	if (!path) { 
+	if (!path) {
 		console_printf("Please set 'tftp-path' var\n");
 		goto errfreets;
 	}
-	if (!file) { 
+	if (!file) {
 		console_printf("Please set 'tftp-file' var\n");
 		goto errfreets;
 	}
-
 
 	tftp_request(u->ts, host, path, file);
 	u->pos = 0;
@@ -155,10 +149,22 @@ errfreeu:
 	return -1;
 }
 
-CONSOLE_CMD(tftp, 1, 1, 
-	    do_tftp, NULL, NULL, 
+
+static int  do_tftp_info(int argc, const char* const* argv)
+{
+	console_printf("Flash area start @ %p len %p bytes\n",
+		fr_fs_flash_offset(), CONFIG_ENV_OFFSET - fr_fs_flash_offset());
+	return 0;
+}
+
+CONSOLE_CMD(tftp_info, 1, 1,
+	    do_tftp_info, NULL, NULL,
+	    "Print info about update possibilities"
+	    HELPSTR_NEWLINE ""
+	);
+
+CONSOLE_CMD(tftp, 1, 1,
+	    do_tftp, NULL, NULL,
 	    "Update firmware over tftp"
 	    HELPSTR_NEWLINE "tftp"
 	);
-
-
